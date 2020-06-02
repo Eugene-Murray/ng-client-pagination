220d661821bc0034db574441b152b4f505969c09
Eugene Murray <eugene.murray@outlook.com>
2020-06-02 17:46:54 +0100

    reference the lib in the example app

--- ng-client-pagination-example/src/app/app.module.ts
+++ ng-client-pagination-example/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgClientPaginationModule } from 'ng-client-pagination';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgClientPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
